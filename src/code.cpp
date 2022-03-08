b30e55ca50affb77d736c128d181c868ab9e24b2
DashaEvstratova <alis131140@gmail.com>
2022-03-08 22:27:24 +0300

    6

--- src/code.cpp
+++ src/code.cpp
@@ -98,8 +98,22 @@ namespace assignment {
 
   // Task 9
   void print_kth_elements(int *arr, int length, int k, std::ostream &os) {
-
-    // Write your code here ...
+    if (arr == nullptr) {
+      os<<"Invalid argument: arr\n";
+    }
+    else if (length<=0){
+      os<<"Invalid argument: length\n";
+    }
+    else if (k<=0){
+      os<<"Invalid argument: k\n";
+    }
+    else{
+      for(int i=0; i<length; i++){
+        if (i%k==0) {
+          os<<arr[i]<<"\t";
+        }
+      }
+    }
   }
 
 }  // namespace assignment
